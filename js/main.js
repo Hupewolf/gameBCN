831a7c1758e3898f2d11454468c6a951722eac82
Ngô Văn Quân <25648131.quan@student.iuh.edu.vn>
2026-06-12 23:40:15 +0700

    update allPage

--- js/main.js
+++
@@ -1,6 +0,0 @@
-import { playerState } from './state/playerState.js';
-import { GameHeader }  from './components/GameHeader.js';
-
-// Export để pages dùng, không tự render ở đây
-// vì mỗi trang có option khác nhau (showHamburger, v.v.)
-export { playerState, GameHeader };
